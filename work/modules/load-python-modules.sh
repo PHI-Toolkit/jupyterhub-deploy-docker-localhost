f507d516b56df94251e4b558bee99c4d495eb0c0
Herman Tolentino MD <herman.tolentino@gmail.com>
2017-07-09 02:46:13 -0400

    added Python modules to Dockerfile-singleuser-scipy

--- work/modules/load-python-modules.sh
+++ work/modules/load-python-modules.sh
@@ -7,6 +7,11 @@ export PIP2_BIN=/opt/conda/envs/python2/bin
 export JUPYTER3_BIN=/opt/conda/bin
 export JUPYTER2_BIN=/opt/conda/envs/python2/bin
 
+sudo apt-get update
+sudo apt-get install -y libgeos-dev
+sudo apt-get clean
+sudo rm -rf /var/lib/apt/lists/*
+
 sudo $PIP3_BIN/pip3 install --upgrade pip
 sudo $PIP3_BIN/pip3 install lightning-python pymongo psycopg2 \
       missingno plotly folium toyplot TextBlob geopandas mplleaflet cartodb \
@@ -29,6 +34,8 @@ sudo $CONDA3_BIN/conda install --yes -c bokeh datashader=0.5.0
 sudo $CONDA3_BIN/conda install --yes -c https://conda.anaconda.org/biocore scikit-bio
 sudo $JUPYTER3_BIN/jupyter nbextension enable --py --sys-prefix ipyleaflet
 sudo $CONDA3_BIN/conda install --yes -c conda-forge jupyter_cms
+sudo $CONDA3_BIN/conda remove --quiet --yes --force qt pyqt
+sudo $CONDA3_BIN/conda clean -tipsy
 
 source activate /opt/conda/envs/python2
 sudo $PIP2_BIN/pip install --upgrade pip
@@ -56,4 +63,9 @@ sudo $CONDA2_BIN/conda install --yes -c anaconda biopython
 sudo $CONDA2_BIN/conda install --yes -c bokeh datashader=0.5.0
 sudo $JUPYTER2_BIN/jupyter nbextension enable --py --sys-prefix ipyleaflet
 sudo $CONDA2_BIN/conda install --yes -c conda-forge jupyter_cms
+source deactivate
+
+source activate /opt/conda/envs/python2
+sudo $CONDA2_BIN/conda remove --quiet --yes --force qt pyqt
+sudo $CONDA2_BIN/conda clean -tipsy
 source deactivate
--- work/modules/load-r-modules.sh
+++ work/modules/load-r-modules.sh
@@ -5,6 +5,8 @@ sudo apt-get install -y libmariadb-client-lgpl-dev
 sudo apt-get install -y libmagick++-dev libcairo2- mesa-common-dev
 sudo apt-get install -y libglu1-mesa-dev
 sudo apt-get install -y libudunits2-dev libgdal-dev
+sudo apt-get clean
+sudo rm -rf /var/lib/apt/lists/*
 
 # conda install
 sudo /opt/conda/bin/conda install --yes -c r r-xml
@@ -12,6 +14,8 @@ sudo /opt/conda/bin/conda install --yes gdal
 sudo /opt/conda/bin/conda install --yes -c bioconda r-plotly=4.5.6
 sudo /opt/conda/bin/conda install --yes -c r r-randomforest=4.6_12 r-e1071=1.6_7 r-rpart=4.1.8 r-xml=3.98_1.5 r-quantmod=0.4_7 \
   r-rvest=0.3.2 r-reshape2=1.4.2 r-tm=0.6_2 r-maps=3.1.1 r-htmlwidgets=0.8
+sudo /opt/conda/bin/conda remove --quiet --yes --force qt pyqt
+sudo /opt/conda/bin/conda clean -tipsy
 
 # install.packages()
 R -e "install.packages(c('text2vec', 'topicmodels', 'quanteda'), dependencies=TRUE, repos='http://cran.rstudio.com/')"
