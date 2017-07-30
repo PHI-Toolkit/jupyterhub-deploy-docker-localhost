98468b247bdb96b064bdc2b2dcd35bb33841e073
Herman Tolentino MD <herman.tolentino@gmail.com>
2017-07-29 22:26:22 -0400

    install script geospatial modules

---
+++ work/modules/install-python-geospatial.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# based on http://mappingandco.com/blog/setting-up-a-geospatial-python-tool-box-with-conda/
+conda install -c conda-forge --yes gdal
+conda install --yes pyproj
+conda install -c http://conda.anaconda.org/rbacher --yes spectral
+conda install --yes pillow
+conda install --yes fiona
+conda install --yes rasterio
+conda install --yes geojson
+conda install -c http://conda.anaconda.org/rsignell --yes geopandas
+conda install --yes pyshp
+conda install -c https://conda.anaconda.org/ioos --yes descartes
+conda install --yes matplotlib
+conda install --yes shapely
+
+source activate python2
+conda install -c conda-forge --yes gdal
+conda install --yes pyproj
+conda install -c http://conda.anaconda.org/rbacher --yes spectral
+conda install --yes pillow
+conda install --yes fiona
+conda install --yes rasterio
+conda install --yes geojson
+conda install -c http://conda.anaconda.org/rsignell --yes geopandas
+conda install --yes pyshp
+conda install -c https://conda.anaconda.org/ioos --yes descartes
+conda install --yes matplotlib
+conda install --yes shapely
+source deactivate
