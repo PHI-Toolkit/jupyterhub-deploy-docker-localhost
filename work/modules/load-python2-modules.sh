900308d8da1497802885a65e83991b894b3f4534
Herman Tolentino MD <herman.tolentino@gmail.com>
2017-08-02 17:52:51 -0400

    module corrections

--- work/modules/load-python2-modules.sh
+++ work/modules/load-python2-modules.sh
@@ -8,7 +8,7 @@ pip install lightning-python pymongo psycopg2 \
   missingno plotly folium toyplot TextBlob geopandas mplleaflet cartodb \
   bubbles python-arango findspark epipy scikit-surprise graphviz cachey \
   sklearn-pandas
-pip install dask[complete] smopy gmplot
+pip install dask[complete] smopy gmplot RISE
 
 jupyter nbextension install https://rawgit.com/jfbercher/jupyter_nbTranslate/master/nbTranslate.zip --user
 jupyter nbextension enable nbTranslate/main
@@ -20,7 +20,6 @@ conda install --yes -c conda-forge geos osmnx gmaps geopy
 conda install --yes -c ioam holoviews=1.8.0 geoviews=1.3.0
 conda install --yes -c r rpy2
 conda install --yes xarray
-conda install -c damianavila82 rise
 
 conda install --yes -c bokeh datashader=0.5.0
 conda install --yes -c conda-forge iris pyshp pandas-datareader obspy basemap \
--- work/modules/load-python3-modules.sh
+++ work/modules/load-python3-modules.sh
@@ -6,7 +6,7 @@ pip3 install lightning-python pymongo psycopg2 \
       missingno plotly folium toyplot TextBlob geopandas mplleaflet cartodb \
       bubbles python-arango findspark epipy scikit-surprise ipynb graphviz cachey \
       sklearn-pandas
-pip3 install dask[complete] smopy gmplot
+pip3 install dask[complete] smopy gmplot RISE
 
 jupyter nbextension install https://rawgit.com/jfbercher/jupyter_nbTranslate/master/nbTranslate.zip --user
 jupyter nbextension enable nbTranslate/main
@@ -18,7 +18,6 @@ conda install --yes -c conda-forge geos osmnx gmaps geopy
 conda install --yes -c ioam holoviews=1.8.0 geoviews=1.3.0
 conda install --yes -c r rpy2
 conda install --yes xarray
-conda install -c damianavila82 rise
 
 conda install --yes -c bokeh datashader=0.5.0
 conda install --yes -c conda-forge iris pyshp pandas-datareader obspy basemap \
