2f1bcf75e775e126e4da073ac1b191c12c28b428
Herman Tolentino MD <herman.tolentino@gmail.com>
2018-08-24 21:12:32 -0400

    Dockerfile.custom Dockerfile.stacks install-docker-bionic.sh

---
+++ install-docker-bionic.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt -y install docker-ce
+sudo systemctl status docker
+sudo usermod -aG docker ${USER}
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+sudo apt-get install make
+print "Please exit console and log back in agan..."
