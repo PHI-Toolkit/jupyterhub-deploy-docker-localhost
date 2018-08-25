10279d8f703399ce3e8a000947aea7afe28fa4c9
Herman Tolentino MD <herman.tolentino@gmail.com>
2018-08-25 07:42:28 -0400

    modified create-certs.sh

--- create-certs.sh
+++ create-certs.sh
@@ -3,6 +3,8 @@
 if [[ ! -d secrets ]]; then
     echo "Creating 'secrets' directory..."
     mkdir -p secrets
+fi
+if [[ ! -f secrets/jupyterhub.pem ]]; then
     echo "Generating self-signed certificates..."
     openssl req -x509 -nodes -days 365 -newkey rsa:1024 \
       -subj "/C=US/ST=GA/L=Atlanta/O=CDC/CN=geeks.io" \
