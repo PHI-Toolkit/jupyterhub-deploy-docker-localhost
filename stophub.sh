ef3a72b0910cb38657fea730fc022b29f269ce2d
Herman Tolentino <herman.tolentino@gmail.com>
2018-10-17 19:11:54 -0400

    updated docker-compose to v3

--- stophub.sh
+++ stophub.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+if [[ "$(docker ps -a | grep jupyter- | awk '{print $1}')" != "" ]]; then
+    docker stop $(docker ps -a | grep jupyter- | awk '{print $1}')
+    docker rm $(docker ps -a | grep jupyter- | awk '{print $1}')
+fi
 docker-compose down
-docker stop $(docker ps -a | grep jupyter- | awk '{print $1}')
-docker rm $(docker ps -a | grep jupyter- | awk '{print $1}')
