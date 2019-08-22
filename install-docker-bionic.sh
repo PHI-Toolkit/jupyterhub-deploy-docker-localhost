6bbd26c306c9ff94c73f4cec6e237e3d3e6b478a
Herman Tolentino <herman.tolentino@gmail.com>
2019-08-21 23:18:22 -0400

    updated install config

--- install-docker-bionic.sh
+++ install-docker-bionic.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source .env
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
@@ -6,7 +7,7 @@ sudo apt update
 apt-cache policy docker-ce
 sudo apt -y install docker-ce
 sudo systemctl status docker
-sudo usermod -aG docker ${USER}
+sudo usermod -aG docker ${INSTALL_USER}
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
