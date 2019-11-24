91d5eeb472c7b124d59a544b8bd4a3ee87195800
Herman Tolentino <herman.tolentino@gmail.com>
2019-11-24 16:20:48 -0500

    created download_cases.sh for cloning cases

---
+++ singleuser/work/shared/download_cases.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd ../notebooks
+git clone https://github.com/PHI-Case-Studies/2018-GEEKS-Digital-Case-Study-Intro.git
+git clone https://github.com/PHI-Case-Studies/1854-Cholera-Outbreak-London-Basic.git
+git clone https://github.com/PHI-Case-Studies/1854-Cholera-Outbreak-London-Advanced-1.git
+git clone https://github.com/PHI-Case-Studies/1854-Cholera-Outbreak-London-Advanced-2.git
+git clone https://github.com/PHI-Case-Studies/2019-HIV-Prevalence-Botswana.git
+
