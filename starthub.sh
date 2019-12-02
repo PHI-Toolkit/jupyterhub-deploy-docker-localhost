c7684e80b5ba63c53faa7c3ebee707d17a6bdcda
Herman Tolentino <herman.tolentino@gmail.com>
2019-12-02 06:17:45 -0500

    updated starthub.sh script - boot up message

--- starthub.sh
+++ starthub.sh
@@ -6,7 +6,10 @@ echo "JUPYTERHUB_SSL = $JUPYTERHUB_SSL"
 case $JUPYTERHUB_SSL in
     use_ssl_ss)
         echo "Starting up JupyterHub..."
-        docker-compose -f docker-compose.yml up -d
+        docker-compose -f docker-compose.yml up
+        echo $?
+        echo "JupyterHub started. Press ctrl-C to stop tracking, JupyterHub will continue running."
+        echo "Run stophub.sh on the command line to stop the JupyterHub server."
         docker-compose -f docker-compose.yml logs -f -t --tail='all'
         ;;
     use_ssl_le)
@@ -17,6 +20,8 @@ case $JUPYTERHUB_SSL in
             sudo cp secrets/$JH_FQDN/key.pem secrets/jupyterhub.key
         fi
         docker-compose -f docker-compose-letsencrypt.yml up -d
+        echo "JupyterHub-LetsEncrypt started. Press ctrl-C to stop tracking, JupyterHub will continue running."
+        echo "Run stophub.sh on the command line to stop the JupyterHub-LetsEncrypt servers."
         docker-compose -f docker-compose-letsencrypt.yml logs -f -t --tail='all'
         ;;
 esac
