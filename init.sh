8285f2debbc59910eeb209278fc655997f0bd4b9
Herman Tolentino <herman.tolentino@gmail.com>
2020-02-05 09:56:16 -0500

    deleted unneeded scripts and apps

--- init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source .env
-#sudo cp secrets/$JH_FQDN/fullchain.pem secrets/jupyterhub.pem
-#sudo cp secrets/$JH_FQDN/key.pem secrets/jupyterhub.key
-docker exec jupyterhub bash -c 'echo JH_FQDN: $JH_FQDN'
-docker exec jupyterhub bash -c 'cp /srv/jupyterhub/secrets/$JH_FQDN/fullchain.pem /srv/jupyterhub/secrets/jupyterhub.pem'
-docker exec jupyterhub bash -c 'cp /srv/jupyterhub/secrets/$JH_FQDN/key.pem /srv/jupyterhub/secrets/jupyterhub.key'
--- remove_secrets.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rm -fvR secrets/archive secrets/keys secrets/csr secrets/live secrets/renewal secrets/renewal-hooks secrets/accounts
-rm secrets/*.key secrets/*.pem 
