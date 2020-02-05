d635ac63488758920ee38ceec3e306af1b3d422a
Herman Tolentino <herman.tolentino@gmail.com>
2020-02-05 09:56:34 -0500

    removed env

--- install-docker-bionic.sh
+++ install-docker-bionic.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source .env-template
+INSTALL_USER=
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
