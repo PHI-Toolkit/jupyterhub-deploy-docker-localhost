672596ae6241a5cede359085a21fd55fb190b92d
Herman Tolentino <herman.tolentino@gmail.com>
2020-02-09 00:45:54 -0500

    changed to direct download for IndexDirectory

--- server_apps/cliff/launch.sh
+++ server_apps/cliff/launch.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Modified 20200208
+
 cd conf
 rm tomcat-users.xml
 wget https://raw.githubusercontent.com/ahalterman/CLIFF-up/master/tomcat-users.xml
@@ -14,24 +16,26 @@ apt-get install -y git maven
 echo "Downloading CLAVIN..."
 cd; git clone https://github.com/Berico-Technologies/CLAVIN.git
 
-echo "Downloading placenames file from Geonames..."
-cd CLAVIN
-wget http://download.geonames.org/export/dump/allCountries.zip
-unzip allCountries.zip
-rm allCountries.zip
+#echo "Downloading placenames file from Geonames..."
+#cd CLAVIN
+#wget http://download.geonames.org/export/dump/allCountries.zip
+#unzip allCountries.zip
+#rm allCountries.zip
 
-echo "Compiling CLAVIN"
-mvn compile
+#echo "Compiling CLAVIN"
+#mvn compile
 
-echo "Building Lucene index of placenames--this is the slow one"
-MAVEN_OPTS="-Xmx8g" mvn exec:java -Dexec.mainClass="com.bericotech.clavin.index.IndexDirectoryBuilder"
+#echo "Building Lucene index of placenames--this is the slow one"
+#MAVEN_OPTS="-Xmx8g" mvn exec:java -Dexec.mainClass="com.bericotech.clavin.index.IndexDirectoryBuilder"
 
-
+wget https://github.com/berkmancenter/mediacloud-clavin-build-geonames-index/releases/download/2019-09-06/IndexDirectory.tar.gz
+tar zxvf IndexDirectory.tar.gz
 mkdir /etc/cliff2
 echo "\n\n"
 echo "THIS IS THE SPOT WHERE THE INDEX DIR GETS COPIED"
 echo "\n\n"
-cp -r IndexDirectory /etc/cliff2/IndexDirectory
+#cp -r IndexDirectory /etc/cliff2/IndexDirectory
+mv IndexDirectory /etc/cliff2/IndexDirectory
 
 cd; cd .m2
 rm settings.xml; wget https://raw.githubusercontent.com/ahalterman/CLIFF-up/master/settings.xml
