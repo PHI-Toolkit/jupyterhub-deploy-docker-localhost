92a9a9c357909f7ab8eb0d6d5a531211a4493047
Herman Tolentino <herman.tolentino@gmail.com>
2021-09-25 19:11:30 -0400

    updated server startup and shutdown scripts

--- restarthub.sh
+++ restarthub.sh
@@ -1,4 +1,3 @@
-##Stop running hub
-sh stophub.sh
-##Start running hub
-sh starthub.sh
+version https://git-lfs.github.com/spec/v1
+oid sha256:ecef00b3a3bf6844d32acfebfb7ecdaec5c504d2b98f764c894a7c56a12333b0
+size 67
